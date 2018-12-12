716983f76dcc4fbb1409259baac4951f759b3576
bsoufiani <basile.soufiani@isep.fr>
2018-12-12 16:30:22 +0100

    Create RatingActivity.java

---
+++ app/src/main/java/com/example/baanu/culte_xchange/RatingActivity.java
@@ -0,0 +1,13 @@
+package com.example.baanu.culte_xchange;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RatingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rating);
+    }
+}
