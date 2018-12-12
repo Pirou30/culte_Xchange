2dd6387a61095cd7db9eadd17a5348e4ea538bdb
Yassmine bouali <bouali.yassmine@gmail.com>
2018-12-12 16:35:14 +0100

    yass

---
+++ app/src/main/java/com/example/baanu/culte_xchange/UrProfile.java
@@ -0,0 +1,13 @@
+package com.example.baanu.culte_xchange;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UrProfile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ur_profile);
+    }
+}
