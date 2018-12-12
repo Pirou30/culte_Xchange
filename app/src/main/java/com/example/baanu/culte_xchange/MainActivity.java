fe776ed39c0217aaeaa6a119a2643c762bf5318d
adamelgamali <adam.elg1@gmail.com>
2018-12-12 15:53:45 +0100

    Some changes have been done: - New icon design - API FACEBOOK - Icon miniature

--- app/src/main/java/com/example/baanu/culte_xchange/MainActivity.java
+++ app/src/main/java/com/example/baanu/culte_xchange/MainActivity.java
@@ -1,13 +1,104 @@
 package com.example.baanu.culte_xchange;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.Login;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class MainActivity extends Activity {
+
+    CallbackManager callbackManager;
+    ProgressDialog mDialog;
+
+    protected void onActivityResult(int requestcode, int resultcode, Intent data){
+        super.onActivityResult(requestcode,resultcode,data);
+        callbackManager.onActivityResult(requestcode,resultcode,data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.connexion);
+        callbackManager= CallbackManager.Factory.create();
+        final LoginButton loginButton = (LoginButton)findViewById(R.id.imageButton4);
+      //  LoginButton.setReadPermissions(Arrays.asList("public_profile","email","user_birthday","user_friends"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+              mDialog=new ProgressDialog(MainActivity.this);
+              mDialog.setMessage("Retrieving data ...");
+              mDialog.show();
+              String accesstoken = loginResult.getAccessToken().getToken();
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        mDialog.dismiss();
+                        getData(object);
+                    }
+                });
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    }
+
+    private void getData(JSONObject object) {
+        try{
+             URL profile_picture = new URL("https://graph.facebook.com/"+object.getString("id"+"/picture?width=250&height=250 "));
+            //Picasso.with(this).load(profile_picture.toString()).into(imgAvatar);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    };
+
+    private void printKeyHash(){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.example.baanu.culte_xchange", PackageManager.GET_SIGNATURES);
+            for (Signature signature:info.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(),Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
