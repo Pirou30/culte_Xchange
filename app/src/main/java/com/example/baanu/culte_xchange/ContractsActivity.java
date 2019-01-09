44d1bf1f2284bfe2f109a46a3c454281f9fc4e2a
bsoufiani <basile.soufiani@isep.fr>
2019-01-09 13:40:25 +0100

    final version of my assignement2

---
+++ app/src/main/java/com/example/baanu/culte_xchange/ContractsActivity.java
@@ -0,0 +1,12 @@
+package com.example.baanu.culte_xchange;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContractsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/baanu/culte_xchange/MembersSeekingActivity2.java
@@ -0,0 +1,13 @@
+package com.example.baanu.culte_xchange;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MembersSeekingActivity2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_members_seeking2);
+    }
+}
